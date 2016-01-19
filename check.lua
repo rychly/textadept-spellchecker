d9c99d0d54ea9d18edb252ccc019cb961ec70eca
xomachine <xomachiner@gmail.com>
2016-01-19 20:51:15 +0300

    Text checking and highlighting routines separated from main file

---
+++ check.lua
@@ -0,0 +1,88 @@
+
+--------------------
+-- Check initiators
+--------------------
+local _M = {}
+local backend = require("textadept-spellchecker.backend")
+
+------------------------
+-- Document highlighting
+------------------------
+local function highlight(word, suggestions)
+  -- Highlights all occurences of given word in buffer with given style
+  if word == nil or word:len() < 2 then
+    return
+  end
+  local style = 3
+  if suggestions then style = 1 end
+  local word_len = word:len()
+  local text = buffer:text_range(0, buffer.length)
+  local pos = 1
+  local last = 1
+  while pos do
+    pos = text:find("[%s%p]"..word.."[%s%p]", last)
+    if pos then
+      last = pos + word_len
+      buffer.indicator_current = style
+      buffer:indicator_fill_range( pos, word_len )
+    end
+  end
+end
+
+local function check_text(text)
+  -- Performs spelling check for supplied visible text
+  local checker = backend.get_checker()
+  local uniq_words = {}
+  -- for word in text:gmatch("[^%s%p][^%s%p]+")
+  for word in text:gmatch("[^%s%p][^%s%p]+")
+  do
+    if not uniq_words[word] and word:len() > 1 then
+      uniq_words[word] = true
+    end
+  end
+  for word,_ in pairs(uniq_words)
+  do
+    checker:write(word.."\n")
+  end
+end
+
+function _M.frame()
+  -- Performs spelling check for visible text
+  local lastline = buffer.first_visible_line + buffer.lines_on_screen
+  local start = buffer:position_from_line(buffer.first_visible_line-1)
+  local finish = buffer:position_from_line(lastline+1)
+  if start == -1 then start = 0 end
+  if finish == -1 then finish = buffer.length end
+  start = buffer:word_start_position(start, false)
+  finish = buffer:word_end_position(finish, false)
+  -- occurs when user scrolls buffer during position measurements
+  if start >= finish then return end 
+  buffer:indicator_clear_range(0, buffer.length)
+  check_text(buffer:text_range(start, finish))
+end
+
+function _M.file()
+  -- Checking whole file in background
+  buffer:indicator_clear_range(0, buffer.length)
+  check_text(buffer:text_range(0, buffer.length))
+end
+
+local function shutdown()
+  -- events.disconnect(events.FILE_AFTER_SAVE, check.file)
+  events.disconnect(backend.ANSWER, highlight)
+  events.disconnect(events.RESET_BEFORE, shutdown)
+  backend.kill_checker()
+  buffer:indicator_clear_range(0, buffer.length)
+end
+
+local function connect_events()
+  -- events.connect(events.FILE_AFTER_SAVE, check.file)
+  events.connect(backend.ANSWER, highlight)
+  events.connect(events.QUIT, shutdown)
+  events.connect(events.RESET_BEFORE, shutdown)
+end
+
+if backend then
+  connect_events()
+  return _M
+end
--- init.lua
+++ init.lua
@@ -1,83 +1,12 @@
 local timer = require("textadept-spellchecker.timer")
-local backend = require("textadept-spellchecker.backend")
 local suggestions = require("textadept-spellchecker.suggestions")
-
-------------------------
--- Document highlighting
-------------------------
-local function highlight(word, suggestions)
-  -- Highlights all occurences of given word in buffer with given style
-  if word == nil or word:len() < 2 then
-    return
-  end
-  local style = 3
-  if suggestions then style = 1 end
-  local word_len = word:len()
-  local text = buffer:text_range(0, buffer.length)
-  local pos = 1
-  local last = 1
-  while pos do
-    pos = text:find("[%s%p]"..word.."[%s%p]", last)
-    if pos then
-      last = pos + word_len
-      buffer.indicator_current = style
-      buffer:indicator_fill_range( pos, word_len )
-    end
-  end
-end
-
-
---------------------
--- Check initiators
---------------------
-local function check_text(text)
-  -- Performs spelling check for supplied visible text
-  local checker = backend.get_checker()
-  local uniq_words = {}
-  -- for word in text:gmatch("[^%s%p][^%s%p]+")
-  for word in text:gmatch("[^%s%p][^%s%p]+")
-  do
-    if not uniq_words[word] and word:len() > 1 then
-      uniq_words[word] = true
-    end
-  end
-  -- Not sure how events work in textadept.
-  -- Reconnect events just in case
-  events.disconnect(backend.ANSWER, highlight)
-  events.connect(backend.ANSWER, highlight)
-  for word,_ in pairs(uniq_words)
-  do
-    checker:write(word.."\n")
-  end
-end
-
-local function check_frame()
-  -- Performs spelling check for visible text
-  local lastline = buffer.first_visible_line + buffer.lines_on_screen
-  local start = buffer:position_from_line(buffer.first_visible_line-1)
-  local finish = buffer:position_from_line(lastline+1)
-  if start == -1 then start = 0 end
-  if finish == -1 then finish = buffer.length end
-  start = buffer:word_start_position(start, false)
-  finish = buffer:word_end_position(finish, false)
-  -- occurs when user scrolls buffer during position measurements
-  if start >= finish then return end 
-  buffer:indicator_clear_range(start, finish)
-  check_text(buffer:text_range(start, finish))
-end
-
-local function check_file()
-  -- Checking whole file in background
-  buffer:indicator_clear_range(0, buffer.length)
-  check_text(buffer:text_range(0, buffer.length))
-end
-
+local check = require("textadept-spellchecker.check")
 
 -------------------------------
 -- Live checking routines
 -------------------------------
 local function on_expire()
-  check_frame()
+  check.frame()
 end
 
 local livecheck_timer = timer.create_timer(on_expire, 2)
@@ -100,23 +29,21 @@ end
 -- Module load/unload routines
 -------------------------------
 local function shutdown()
-  --events.disconnect(events.FILE_AFTER_SAVE, check_file)
   events.disconnect(events.RESET_BEFORE, shutdown)
   events.disconnect(events.UPDATE_UI, on_activity)
   events.disconnect(events.KEYPRESS, on_keypress)
   suggestions.disconnect_events()
-  backend.kill_checker()
-  buffer:indicator_clear_range(0, buffer.length)
 end
 local function connect_events()
-  --events.connect(events.FILE_AFTER_SAVE, check_file)
   events.connect(events.QUIT, shutdown)
   events.connect(events.RESET_BEFORE, shutdown)
   events.connect(events.UPDATE_UI, on_activity)
   events.connect(events.KEYPRESS, on_keypress)
-  suggestions.connect_events()
+  if suggestions then
+    suggestions.connect_events()
+  end
 end
 
-if backend then
+if check then
   connect_events()
 end
