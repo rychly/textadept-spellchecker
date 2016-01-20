4f8b63486531dfd5b1a1dc061abbd88e56b58b3b
xomachine <xomachiner@gmail.com>
2016-01-20 20:40:59 +0300

    Livechecking routines separated from main file

--- init.lua
+++ init.lua
@@ -1,39 +1,16 @@
 local backend = require("textadept-spellchecker.backend")
-local timer = require("textadept-spellchecker.timer")
 local suggestions = require("textadept-spellchecker.suggestions")
 local check = require("textadept-spellchecker.check")
 local gui = require("textadept-spellchecker.gui")
+local live = require("textadept-spellchecker.livechecking")
 
--------------------------------
--- Live checking routines
--------------------------------
-local function on_expire()
-  check.frame()
-end
 
-local livecheck_timer = timer.create_timer(on_expire, 2)
-
-local function hasbit(x, bit)
-  return x % (bit + bit) >= bit
-end
-
-local function on_activity(updated)
-  if updated and hasbit(updated, buffer.UPDATE_V_SCROLL) then
-    timer.start(livecheck_timer)
-  end
-end
-
-local function on_keypress()
-  timer.start(livecheck_timer)
-end
 
 -------------------------------
 -- Module load/unload routines
 -------------------------------
 local function shutdown()
   events.disconnect(events.RESET_BEFORE, shutdown)
-  events.disconnect(events.UPDATE_UI, on_activity)
-  events.disconnect(events.KEYPRESS, on_keypress)
 end
 
 local function init()
@@ -45,15 +22,15 @@ local function init()
   events.connect(events.QUIT, shutdown)
   -- TODO: Investigate why shutdown causes calling nil value
   -- events.connect(events.RESET_BEFORE, shutdown)
-  events.connect(events.UPDATE_UI, on_activity)
-  events.connect(events.KEYPRESS, on_keypress)
   if suggestions then
     suggestions.init()
   end
   if gui then
     gui.init()
   end
-  on_keypress() -- Start spellchecking after loading
+  if live then
+    live.init()
+  end
 end
 
 events.connect(events.INITIALIZED, init)
---
+++ livechecking.lua
@@ -0,0 +1,43 @@
+local check = require("textadept-spellchecker.check")
+local timer = require("textadept-spellchecker.timer")
+-------------------------------
+-- Live checking routines
+-------------------------------
+local _M = {}
+
+local function on_expire()
+  check.frame()
+end
+
+local livecheck_timer = timer.create_timer(on_expire, 2)
+
+local function hasbit(x, bit)
+  return x % (bit + bit) >= bit
+end
+
+local function on_activity(updated)
+  if updated and hasbit(updated, buffer.UPDATE_V_SCROLL) then
+    timer.start(livecheck_timer)
+  end
+end
+
+local function on_keypress()
+  timer.start(livecheck_timer)
+end
+
+function _M.shutdown()
+  events.disconnect(events.RESET_BEFORE, _M.shutdown)
+  events.disconnect(events.UPDATE_UI, on_activity)
+  events.disconnect(events.KEYPRESS, on_keypress)
+end
+
+function _M.init()
+  events.connect(events.QUIT, _M.shutdown)
+  -- TODO: Investigate why shutdown causes calling nil value
+  -- events.connect(events.RESET_BEFORE, shutdown)
+  events.connect(events.UPDATE_UI, on_activity)
+  events.connect(events.KEYPRESS, on_keypress)
+  on_keypress() -- Start spellchecking after loading
+end
+
+return _M
